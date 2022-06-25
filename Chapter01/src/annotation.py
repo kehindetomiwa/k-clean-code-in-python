9ea0ebbf4186bb020a152dac96f8431f6eda091f
kehindetomiwa <kehinde.tomiwa4@gmail.com>
2022-06-25 23:35:15 +0200

    adding formatter

--- Chapter01/src/annotation.py
+++ Chapter01/src/annotation.py
@@ -1,11 +1,12 @@
 class Point:  # pylint: disable=R0903
     """Example to be used as return type of locate"""
+
     def __init__(self, lat, long):
         self.lat = lat
         self.long = long
 
 
-def locate(latitude, longitude):
+def locate(latitude, longitude) -> Point:
     """Find an object in the map by its coordinates"""
     return Point(latitude, longitude)
 
@@ -13,3 +14,4 @@ def locate(latitude, longitude):
 class NewPoint:  # pylint: disable=R0903
     """Example to display its __annotations__ attribute."""
     lat: float
+    long: float
--- Chapter01/src/test.py
+++ Chapter01/src/test.py
@@ -3,4 +3,4 @@ def my_function(name):
     >>> my_function('black')
     'received Black'
     """
-    return 'received {0}'.format(name.title())
+    return "received {0}".format(name.title())
